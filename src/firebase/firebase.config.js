259f9fd66f3da6c5f96a88e00c36339df9569f88
unknown <ekaramulhasan063@gmail.com>
2022-11-16 08:32:45 +0600

    setup firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfieWR9c84PJmTwYAPoOAih2bYgjl9ng4",
+  authDomain: "travel-care-c3001.firebaseapp.com",
+  projectId: "travel-care-c3001",
+  storageBucket: "travel-care-c3001.appspot.com",
+  messagingSenderId: "349659329737",
+  appId: "1:349659329737:web:fac4e58ee9e6b757334918"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
